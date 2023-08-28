fc4dc0d1ec7141b57c040da68460682e2d6680ba
AmrSalah12 <amrsalahshahin@gmail.com>
2023-08-28 21:49:05 +0300

    Add Validate Pricelist Module

---
+++ validate_pricelist/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Validate Pricelist",
+
+    'summary': """
+        Validate Pricelists for Selected Sales Teams
+    """,
+
+    'description': """
+        Validate Pricelists for Selected Sales Teams
+    """,
+
+    'author': "AMR SALAH",
+    'license': 'OPL-1',
+    'version': '14.0',
+
+    'depends': ['sale', 'sales_team', 'crm', 'product'],
+
+    'data': [
+        'views/pricelist.xml',
+    ],
+}
